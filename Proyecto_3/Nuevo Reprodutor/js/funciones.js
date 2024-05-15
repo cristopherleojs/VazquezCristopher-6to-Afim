1081c61aed3f46d043041137a6c1ce3b288498e1
cristopherleonardo <cristopherleonardo96@gmail.com>
2024-05-14 19:15:31 -0600

    add div y botones

--- Proyecto_3/Nuevo Reprodutor/js/funciones.js
+++ Proyecto_3/Nuevo Reprodutor/js/funciones.js
@@ -1,5 +1,8 @@
 $(document).ready(function()
 {
+
+let playing_song = false;
+let play = document.querySelector('#play');
 		$(".playlist").audioControls(
 		{
 			autoplay:false,
@@ -11,5 +14,33 @@ $(document).ready(function()
 			 
 				
 
-	})
+	});
+		$(".repro").audioControls(
+		{
+			
+			autoPlay:true,
+			playPauseAudio:function(playAudio)
+			{
+				$(".repro").text(playAudio);
+			}
+        
+		});
+			function justplay(){
+				if (playing_song==false) {
+					playsong();
+				}else{
+					pausesong();
+				}
+			}
+		
+		function playsong(){
+			track.play();
+			playing_song = true;
+			play.innerHTML = '<i class="fa-solid fa-pause"></i>';
+		}
+    function pausesong(){
+			track.pause();
+			playing_song = false;
+			play.innerHTML = '<i class="fa-solid fa-play"></i>';
+		}
 });
